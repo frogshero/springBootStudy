5ab55d2a5a553fe99cfc2b44f4334c56d6ba1174
wanggu <guofu.wang@dianrong.com>
2019-02-18 13:39:25 +0800

    test

--- src/main/java/com/example/demo/AppContextInitializer.java
+++ src/main/java/com/example/demo/AppContextInitializer.java
@@ -7,15 +7,28 @@ import org.springframework.core.io.support.ResourcePropertySource;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * initialize:21, AppContextInitializer (com.example.demo)
+ * applyInitializers:633, SpringApplication (org.springframework.boot)
+ * prepareContext:373, SpringApplication (org.springframework.boot)
+ * run:325, SpringApplication (org.springframework.boot)
+ * run:1255, SpringApplication (org.springframework.boot)
+ * run:1243, SpringApplication (org.springframework.boot)
+ * main:30, DemoApplication (com.example.demo)
+ */
+
 @Slf4j
 public class AppContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
 	public AppContextInitializer() {
-		// TODO Auto-generated constructor stub
+		//public SpringApplication(ResourceLoader resourceLoader, Class... primarySources)
+		//this.setInitializers(this.getSpringFactoriesInstances(ApplicationContextInitializer.class));
+		log.info("-------------------AppContextInitializer");
 	}
 
 	@Override
 	public void initialize(ConfigurableApplicationContext applicationContext) {
+		log.info("-------------------initialize");
 		MutablePropertySources sources = applicationContext.getEnvironment().getPropertySources();
 		try {
 		    ResourcePropertySource props = new ResourcePropertySource("MY_PROPS", "classpath:my.properties");
--- src/main/java/com/example/demo/AppListener.java
+++ src/main/java/com/example/demo/AppListener.java
@@ -1,11 +1,17 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 
+@Slf4j
 public class AppListener implements ApplicationListener<ApplicationEvent> {
 
-	//org.springframework.context.ApplicationListener=com.example.demo.AppListener
+	public AppListener() {
+		//public SpringApplication(ResourceLoader resourceLoader, Class... primarySources)
+		//this.setListeners(this.getSpringFactoriesInstances(ApplicationListener.class));
+		log.info("-------------------AppListener");
+	}
 	
 	@Override
 	public void onApplicationEvent(ApplicationEvent event) {
